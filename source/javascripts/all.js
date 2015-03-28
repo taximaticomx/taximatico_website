74f024e5441e4760e8892ae61be183dd17e9943b
Antonio Chavez <cavjzz@gmail.com>
2015-03-28 15:23:14 -0600

    Remove JavaScript from page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,3 +2,34 @@
 //= require move-top
 //= require easing
 //= require owl.carousel
+
+$(function() {
+  $().UItoTop({ easingType: 'easeOutQuart' });
+
+  $("#owl-demo").owlCarousel({
+    items : 1,
+    lazyLoad : true,
+    autoPlay : true,
+    navigation : false,
+    navigationText :  false,
+    pagination : true,
+  });
+
+  $("#owl-demo1").owlCarousel({
+    items : 1,
+    lazyLoad : true,
+    autoPlay : true,
+    navigation : false,
+    navigationText :  false,
+    pagination : true,
+  });
+
+  $("#owl-demo2").owlCarousel({
+    items : 1,
+    lazyLoad : true,
+    autoPlay : true,
+    navigation : false,
+    navigationText :  false,
+    pagination : true,
+  });
+});
